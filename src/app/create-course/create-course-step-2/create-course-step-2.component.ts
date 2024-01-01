9e74381792e472b7cd15a094262de2c6212501f0
piotr-skolorzynski <p.skolorzynski@gmail.com>
2024-01-01 15:54:25 +0100

    multi stepper to add course -  format code replace constructors with inject function

--- src/app/create-course/create-course-step-2/create-course-step-2.component.ts
+++ src/app/create-course/create-course-step-2/create-course-step-2.component.ts
@@ -1,52 +1,45 @@
-import {Component, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {createPromoRangeValidator} from '../../validators/date-range.validator';
-
+import { Component, OnInit, inject } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+import { createPromoRangeValidator } from "../../validators/date-range.validator";
 
 @Component({
-  selector: 'create-course-step-2',
-  templateUrl: 'create-course-step-2.component.html',
-  styleUrls: ['create-course-step-2.component.scss']
+  selector: "create-course-step-2",
+  templateUrl: "create-course-step-2.component.html",
+  styleUrls: ["create-course-step-2.component.scss"],
 })
 export class CreateCourseStep2Component implements OnInit {
+  private readonly fb = inject(FormBuilder);
 
-    form = this.fb.group({
-        courseType: ['premium', Validators.required],
-        price: [null, [
-            Validators.required,
-            Validators.min(1),
-            Validators.max(9999),
-            Validators.pattern("[0-9]+")
-        ]],
-        thumbnail: [null],
-        promoStartAt: [null],
-        promoEndAt: [null]
-    }, {
-        validators: [createPromoRangeValidator()]
-    });
-
-    constructor(private fb: FormBuilder) {
-
+  form = this.fb.group(
+    {
+      courseType: ["premium", Validators.required],
+      price: [
+        null,
+        [
+          Validators.required,
+          Validators.min(1),
+          Validators.max(9999),
+          Validators.pattern("[0-9]+"),
+        ],
+      ],
+      thumbnail: [null],
+      promoStartAt: [null],
+      promoEndAt: [null],
+    },
+    {
+      validators: [createPromoRangeValidator()],
     }
+  );
 
   ngOnInit() {
+    this.form.valueChanges.subscribe((val) => {
+      const priceControl = this.form.controls["price"];
 
-        this.form.valueChanges
-            .subscribe(val => {
-
-                const priceControl = this.form.controls["price"];
-
-                if (val.courseType == 'free' && priceControl.enabled) {
-                    priceControl.disable({emitEvent: false});
-                }
-                else if (val.courseType == 'premium' && priceControl.disabled) {
-                    priceControl.enable({emitEvent:false});
-                }
-
-            });
-
-
-
+      if (val.courseType == "free" && priceControl.enabled) {
+        priceControl.disable({ emitEvent: false });
+      } else if (val.courseType == "premium" && priceControl.disabled) {
+        priceControl.enable({ emitEvent: false });
+      }
+    });
   }
-
 }
--- src/app/create-course/create-course-step-3/create-course-step-3.component.ts
+++ src/app/create-course/create-course-step-3/create-course-step-3.component.ts
@@ -1,44 +1,32 @@
-import {Component} from '@angular/core';
-import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
-
+import { Component, inject } from "@angular/core";
+import { FormArray, FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'create-course-step-3',
-  templateUrl: 'create-course-step-3.component.html',
-  styleUrls: ['create-course-step-3.component.scss']
+  selector: "create-course-step-3",
+  templateUrl: "create-course-step-3.component.html",
+  styleUrls: ["create-course-step-3.component.scss"],
 })
 export class CreateCourseStep3Component {
+  private readonly fb = inject(FormBuilder);
 
-    form = this.fb.group({
-        lessons: this.fb.array([])
+  form = this.fb.group({
+    lessons: this.fb.array([]),
+  });
+
+  get lessons() {
+    return this.form.controls["lessons"] as FormArray;
+  }
+
+  addLesson() {
+    const lessonForm = this.fb.group({
+      title: ["", Validators.required],
+      level: ["beginner", Validators.required],
     });
 
+    this.lessons.push(lessonForm);
+  }
 
-    constructor(private fb:FormBuilder) {
-
-    }
-
-    get lessons() {
-        return this.form.controls["lessons"] as FormArray;
-    }
-
-    addLesson() {
-
-        const lessonForm = this.fb.group({
-            title: ['', Validators.required],
-            level: ['beginner', Validators.required]
-        });
-
-        this.lessons.push(lessonForm);
-    }
-
-    deleteLesson(lessonIndex: number) {
-        this.lessons.removeAt(lessonIndex);
-    }
+  deleteLesson(lessonIndex: number) {
+    this.lessons.removeAt(lessonIndex);
+  }
 }
-
-
-
-
-
-
